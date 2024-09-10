a5cd58f3f560ba5aab9f01c2d722e7ea7560c4fa
manuelota <134301866+manuelota@users.noreply.github.com>
2024-09-10 20:15:59 -0300

    recuperar contra
    oa

--- src/app/pages/recuperar/recuperar.page.ts
+++ src/app/pages/recuperar/recuperar.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-recuperar',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecuperarPage implements OnInit {
 
-  constructor() { }
+  
+  email:String ="";
+  password:String ="";
+
+  constructor(private alertController: AlertController) { }
 
   ngOnInit() {
   }
 
+  async recoverPassword() {
+    if (this.email === "runfama@gmail.com") {
+      const alert = await this.alertController.create({
+        header: 'Contraseña Recuperada',
+        message: 'Tu contraseña es: runfama',
+        buttons: ['OK']
+      });
+      await alert.present();
+    } else {
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'no se puedo recuperar la contraseña .',
+        buttons: ['OK']
+      });
+      await alert.present();
+    }
+  }
+    
 }
