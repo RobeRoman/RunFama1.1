701ea195c8ddcd1f3aab051914632e71413958fd
JanoAle <137423726+RobeRoman@users.noreply.github.com>
2024-09-23 16:10:56 -0300

    Arreglo de recuperar

--- src/app/pages/recuperar/recuperar.page.ts
+++ src/app/pages/recuperar/recuperar.page.ts
@@ -1,6 +1,7 @@
-import { Attribute, Component, input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
+import { UsuarioService } from '../../services/usuario.service';
 
 @Component({
   selector: 'app-recuperar',
@@ -9,17 +10,21 @@ import { AlertController } from '@ionic/angular';
 })
 export class RecuperarPage implements OnInit {
 
-  email:String ="";
-  constructor(private alertController: AlertController, private router: Router) { }
+  email: string = "";
 
-  ngOnInit() {
-  }
-  //ñ TENGO TECLADO EN INGLES xd
+  constructor(
+    private alertController: AlertController,
+    private router: Router,
+    private usuarioService: UsuarioService // Inyectar UsuarioService
+  ) { }
+
+  ngOnInit() { }
+
   async validarcorreo() {
-    if (this.email === ''){
-      await this.presentAlert('Error', 'Correo vacio');
-    }else if (this.email.includes('@duocuc.cl'))  {
-      await this.presentAlert('Bien', 'Ingrese codigo de verificacion y cambie contraseñas');
+    if (this.email === '') {
+      await this.presentAlert('Error', 'Correo vacío');
+    } else if (this.usuarioService.getUsuarios().some(user => user.correo === this.email)) {
+      await this.presentAlert('Bien', 'Ingrese código de verificación y cambie contraseñas');
       this.router.navigate(['/cambiarclave']);
     } else {
       await this.presentAlert('Error', 'El correo no es válido');
