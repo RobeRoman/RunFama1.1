c0fa3189269119f5aed9376eb733d069e531249f
JanoAle <137423726+RobeRoman@users.noreply.github.com>
2024-10-20 18:26:31 -0300

    Correcion

--- src/app/pages/reservas/reservas.page.ts
+++ src/app/pages/reservas/reservas.page.ts
@@ -15,18 +15,18 @@ import 'leaflet-routing-machine';
   styleUrls: ['./reservas.page.scss'],
 })
 export class ReservasPage implements OnInit {
-  id: number = 1;
+  
   usuario: any;
   private map: L.Map | undefined;
   private geocoder: G.Geocoder | undefined;
 
   viaje = new FormGroup({
-    id: new FormControl(this.id),
+    id: new FormControl(),
     asientos_disponible: new FormControl('',[Validators.required, Validators.min(1), Validators.max(16)]),
     destino: new FormControl('',[Validators.required]),
     latitud: new FormControl('',[Validators.required]),
     longitud: new FormControl('',[Validators.required]),
-    distancia_m: new FormControl('',[]),
+    distancia_m: new FormControl('',[Validators.required]),
     tiempo_minutos: new FormControl(),
     precio: new FormControl(),
     estado: new FormControl('pendiente'),
@@ -87,7 +87,7 @@ export class ReservasPage implements OnInit {
         }).on('routesfound', (e) => {
           // Actualizar el formulario con los detalles de la ruta
           this.viaje.controls.distancia_m.setValue(e.routes[0].summary.totalDistance);
-          this.viaje.controls.tiempo_minutos.setValue(Math.round(e.routes[0].summary.totalTime / 60));
+          this.viaje.controls.tiempo_minutos.setValue(Math.round(e.routes[0].summary.totalTime/60));
           this.viaje.controls.precio.setValue(new Intl.NumberFormat('es-CL', { style: 'currency', currency: 'CLP' }).format(Math.round(e.routes[0].summary.totalDistance / 1000) * 500));
 
         }).addTo(this.map);
