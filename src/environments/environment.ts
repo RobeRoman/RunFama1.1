2ce5fbad969e3164d315dae4bc8cdd361afe9e49
JanoAle <137423726+RobeRoman@users.noreply.github.com>
2024-11-16 16:38:28 -0300

    Se anade la key de bd y el service

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDz5okRqFb3m-03qoKmFUyLJ7aq1Mgmjh0",
+    authDomain: "runfama-2f7f3.firebaseapp.com",
+    projectId: "runfama-2f7f3",
+    storageBucket: "runfama-2f7f3.firebasestorage.app",
+    messagingSenderId: "315125427323",
+    appId: "1:315125427323:web:c436cd991754da297706de"
+  }
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
