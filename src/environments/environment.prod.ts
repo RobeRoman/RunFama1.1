26a24aba4d300b4bb57996ce06c5bfefb22aae75
JanoAle <137423726+RobeRoman@users.noreply.github.com>
2024-12-01 18:40:27 -0300

    Listo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDz5okRqFb3m-03qoKmFUyLJ7aq1Mgmjh0",
+    authDomain: "runfama-2f7f3.firebaseapp.com",
+    projectId: "runfama-2f7f3",
+    storageBucket: "runfama-2f7f3.firebasestorage.app",
+    messagingSenderId: "315125427323",
+    appId: "1:315125427323:web:c436cd991754da297706de"
+  }
 };
